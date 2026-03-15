ddb5ff34861ed0af4d94d74af02187c0ee76daf7
Najiel Alja <najielalja2002@gmail.com>
2026-03-15 13:15:40 +0000

    python

---
+++ basics/01_variables.py
@@ -0,0 +1,16 @@
+age = 25
+name = "John Doe"
+is_married = True
+print(f"my Age is {age}, my Name is {name}, Married: {is_married}")
+
+
+a = 5
+b= 10
+c = 0
+
+c= a
+a = b
+b = c
+print(f"a= {a}, b= {b}")
+a,b = b,a
+print(f"a= {a}, b= {b}")
